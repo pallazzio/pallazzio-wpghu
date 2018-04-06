4bc6935978e5c4d319ccfd9883dc7821e9b89d21
Jeremy Kozan <jeremyakozan@gmail.com>
2018-04-05 21:44:12 -0700

    Beta 1

--- pallazzio-wpghu.php
+++ pallazzio-wpghu.php
@@ -66,7 +66,7 @@ class Pallazzio_WPGHU {
 
 		$last_github_call_time = get_option( $this->github_user . '_' . $this->github_repo . '_Pallazzio_WPGHU_Time' );
 
-		if ( $last_github_call_time && time() - $last_github_call_time < /*60 * 60 * */6 ) { // don't query github more than once every six hours
+		if ( $last_github_call_time && time() - $last_github_call_time < 60 * 60 * 6 ) { // don't query github more than once every six hours
 
 			if ( ! empty( get_option( $this->github_user . '_' . $this->github_repo . '_Pallazzio_WPGHU' ) ) ) {
 
